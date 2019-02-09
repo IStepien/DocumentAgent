d821d60d2d1b91e9351122369fd42d58e74b071c
Irmina Stepien <irm.stepien@gmail.com>
2019-02-09 13:44:59 +0100

    File downloading added, working

--- src/main/java/com/istepien/controller/DocumentController.java
+++ src/main/java/com/istepien/controller/DocumentController.java
@@ -4,9 +4,11 @@ import com.istepien.model.Document;
 import com.istepien.model.User;
 import com.istepien.service.DocumentService;
 import com.istepien.service.UserService;
+import org.h2.util.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,7 +18,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.Principal;
 import java.util.List;
 
@@ -80,19 +85,32 @@ public class DocumentController {
     }
 
     @GetMapping("/showDocumentPreview")
-    public String showDocumentPreview(@RequestParam(name = "docId") Long docId) {
+    public ModelAndView showDocumentPreview(@RequestParam(name = "docId") Long docId) {
         ModelAndView view = new ModelAndView("file-preview");
         view.addObject("docId", docId);
 
-        return "file-preview";
+        return view;
     }
-    @RequestMapping(value = "/show/{docId}", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @GetMapping(value = "/show/{docId}", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<byte[]> getImage(@PathVariable("docId") Long docId) throws IOException {
 
         byte[] docContent = documentService.getDocument(docId).getFile();
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
         return new ResponseEntity<byte[]>(docContent, headers, HttpStatus.OK);
+    }
+    @GetMapping(value = "/download")
+        public void getFile(@RequestParam(name = "docId") Long docId, HttpServletResponse response){
+        try {
+            DefaultResourceLoader loader = new DefaultResourceLoader();
+            InputStream is = new ByteArrayInputStream(documentService.getDocument(docId).getFile());
+            IOUtils.copy(is, response.getOutputStream());
+            response.setHeader("Content-Disposition", "attachment; filename=Accepted.pdf");
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new RuntimeException("IOError writing file to output stream");
+        }
+
     }
 
 }
