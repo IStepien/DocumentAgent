5326d6a28333d0640dfbecc25886f0ca2c112818
Irmina Stepien <irm.stepien@gmail.com>
2019-03-30 22:48:02 +0100

    Logging blocked for banned user

--- src/main/java/com/istepien/service/UserDetailsServiceImpl.java
+++ src/main/java/com/istepien/service/UserDetailsServiceImpl.java
@@ -3,8 +3,10 @@ package com.istepien.service;
 import com.istepien.dao.UserDao;
 import com.istepien.model.Role;
 import com.istepien.model.User;
+import com.sun.deploy.security.BlockedException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,13 +27,17 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException   {
 
         User user = userDao.getUserByName(username);
 
         if(user == null){
             throw new UsernameNotFoundException("User not found");
         }
+        if(user.getIsBlocked() == true){
+            throw new InternalAuthenticationServiceException("Blocked user");
+        }
+
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
 
